a0df8bc4c1eecf2cbb0985a8ada8a32751d5adba
PAMMY100 <princesamuel636@gmail.com>
2024-06-23 04:36:54 +0100

    quick fix

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const jwt = require("jsonwebtoken");
-const multer = require('multer')
 const path = require("path");
 const cors = require("cors");
 const { type } = require('os');
@@ -38,15 +37,6 @@ app.get("/", (req, res) => {
   res.send("Express App is Running")
 })
 
-// Image Storage Engine
-const storage = multer.diskStorage({
-  destination: './upload/images',
-  filename:(req,file,cb) => {
-    return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-  }
-})
-
-const upload = multer({storage:storage})
 
 // Creating Upload Endpoint for images
 app.use('/images', express.static('upload/images'))
